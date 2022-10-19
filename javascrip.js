8e53dc6d64175a00d8181c8f67330e47352b326d
Aqeel Ahamed <112056007+AhamedAqeel@users.noreply.github.com>
2022-10-19 11:14:36 +0530

    Add files via upload

---
+++ javascrip.js
@@ -0,0 +1,8 @@
+function checkpassword(){
+    let password=document.getElementById("pwd").value;
+    let cpassword=document.getElementById("cpwd").value;
+    let msd=document.getElementById("message");
+}
+if (password!=cpassword){
+    document.getElementById("message").innerHTML;
+}
